a1185562de09f880f07cd77aefae75d6f03b6286
Disha Bhat <97371755+dishaabhat@users.noreply.github.com>
2024-10-12 19:45:21 +0530

    Delete tyr.py

--- tyr.py
+++
@@ -1,9 +0,0 @@
-import sys
-import subprocess
-
-# Install ReportLab using pip
-try:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pytesseract'])
-    print("ReportLab installed successfully.")
-except subprocess.CalledProcessError:
-    print("Error occurred while installing ReportLab.")
